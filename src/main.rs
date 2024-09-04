62ab4a0e559d4c8aeea05d37f475324dec0e8ce7
ammernico <nico-steinle@t-online.de>
2024-09-04 18:23:02 +0200

    Set the log level to error

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,7 @@ mod record;
 
 fn main() -> Result<(), anyhow::Error> {
     let subscriber = FmtSubscriber::builder()
-        .with_max_level(Level::TRACE)
+        .with_max_level(Level::ERROR)
         .finish();
     tracing::subscriber::set_global_default(subscriber).expect("setting default subscriber failed");
 
